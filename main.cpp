a808e459cd5277246c48f69c3eb6b3538a82da67
Julien DELSOL <jdelsol@catie.local>
2018-01-15 10:31:57 +0100

    delete led off instruction, add bool enable_alert and alert management routines

--- main.cpp
+++ main.cpp
@@ -16,7 +16,6 @@
  */
 #include "mbed.h"
 #include "max17201.h"
-#include "swo.h"
 #include "Thread.h"
 
 using namespace sixtron;
@@ -25,20 +24,18 @@ namespace {
 #define PERIOD_MS 2000
 }
 
-
-I2C i2c(I2C_SDA, I2C_SCL);
-static DigitalOut led1(LED1);
-MAX17201 gauge(&i2c, DIO1);
-
 // this functions/parameters are used for max17201 management alert
 void print_alrt(uint16_t reg);
 void listener_alrt(void);
 Thread thread_alrt;
 EventQueue queue;
-InterruptIn max17201alrt1(DIO1);
 uint16_t max17201_alrtStatus = 0;
+bool enable_alert = true; //
 
-Serial serial(SERIAL_RX, SERIAL_TX);
+I2C i2c(I2C_SDA, I2C_SCL);
+static DigitalOut led1(LED1);
+
+MAX17201 gauge(&i2c, DIO1);
 
 // main() runs in its own thread in the OSvv
 // (note the calls to Thread::wait below for delays)
@@ -47,21 +44,31 @@ int main()
     i2c.frequency(400000);
     Thread::wait(2000);
 
-    led1 = 0; // led off
-
     /* When configured, the gauge looses all the learning done between last
     configuration. So if gauge remained powered since last configuration,
     dont use the configuration function to keep the learning in order to have more
     accurate values.
     if the last parameter = true (enable_alert), it's necessary to configure the alert threshold value
     in gauge.configure function */
-    if (gauge.configure(1, 800, 3.3, false, false, true)){
+    if (gauge.configure(1, 800, 3.3, false, false)){
 
-        printf("Gauge configured !\n");
+    	printf("Gauge configured !\n");
 
-        // to use alert management with interrupt system you need to use this mechanism
-        thread_alrt.start(callback(&queue, &EventQueue::dispatch_forever));
-        max17201alrt1.fall(queue.event(&listener_alrt));
+        if (enable_alert){
+           	// here, set the alert threshold function
+        	gauge.set_temperature_alerts(MAX_TEMPERATURE_ALERT, MIN_TEMPERATURE_ALERT);
+        	gauge.set_voltage_alerts(MAX_VOLTAGE_ALERT, MIN_VOLTAGE_ALERT);
+        	// max17201 alert enable
+        	gauge.enable_alerts();
+        	gauge.enable_temperature_alerts();
+        	gauge.get_interruptPin()->enable_irq(); // interruption enable
+        	wait_ms(250); // let time to software to compute new values
+
+        	// to use alert management with interrupt system you need to use this mechanism
+			thread_alrt.start(callback(&queue, &EventQueue::dispatch_forever));
+			gauge.get_interruptPin()->fall(queue.event(&listener_alrt));
+        }
+
 
     }
     else {
@@ -69,8 +76,6 @@ int main()
     }
 
     while (true) {
-
-
         printf("Alive!\n");
         printf("Capacity : %.3f mAh\n", gauge.reported_capacity());
         printf("Full Capacity : %.3f mAh\n", gauge.full_capacity());
@@ -78,11 +83,8 @@ int main()
         printf("Voltage : %.3f Volts\n",gauge.cell_voltage()/1000);
         printf("Current : %.3f mA\n",gauge.current());
         printf("Temperature : %.3f\n", gauge.temperature());
-
         led1 = !led1;
         Thread::wait(PERIOD_MS);
-
-
     }
 }
 
@@ -101,10 +103,8 @@ void listener_alrt()
 	// printf for debug : "\r\n" allow to separate this content with main thread contents
 	printf("\r\n");
 	printf("/!\\ alert detected /!\\\n");
-
 	max17201_alrtStatus = gauge.status();
 	print_alrt(max17201_alrtStatus);
-
 	printf("\r\n");
 
 }
@@ -119,14 +119,11 @@ void listener_alrt()
  ******************************************************************/
 void print_alrt(uint16_t res)
 {
-	uint16_t temp = 0;
 	/* treatment status : for each bit of i2c register status (0x00)
 	 * cf page 65 of max17201 datasheet */
-	for (int i=((sizeof(res)*8)-1); i>-1; i--)
-	{
+	for (int i=((sizeof(res)*8)-1); i>-1; i--){
 		// if bit = 1 detected
-		if ((res & (1 << i)))
-		{
+		if ((res & (1 << i))){
 			switch(static_cast<MAX17201::StatusAlert>(i))
 			{
 				// Power On Reset Indicator
@@ -200,9 +197,7 @@ void print_alrt(uint16_t res)
 					break;
 			 }
 		 }
-
 	}
-
 }
 
 
