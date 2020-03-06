8c7adb18fc4997f9be88d5ae264f3815a689c003
Bruno Perraudin <b.perraudin@catie.fr>
2020-03-06 15:44:02 +0000

    Use latest wait API

--- main.cpp
+++ main.cpp
@@ -1,16 +1,16 @@
 /*
- * Copyright (c) 2016, CATIE, All Rights Reserved
+ * Copyright (c) 2016-2020, CATIE
  * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
@@ -43,7 +43,7 @@ static DigitalOut led1(LED1);
 int main()
 {
     i2c.frequency(400000);
-    Thread::wait(2000);
+    ThisThread::sleep_for(2000);
 
     /* When configured, the gauge looses all the learning done between last
     configuration. So if gauge remained powered since last configuration,
@@ -58,7 +58,7 @@ int main()
         // Enable gauge alerts
         gauge.enable_alerts();
         gauge.enable_temperature_alerts();
-        wait_ms(250);
+        ThisThread::sleep_for(250);
         // attach callback alert interrupt function
         gauge.alert_callback(&on_alert);
         // The Event Queue run in its own thread
